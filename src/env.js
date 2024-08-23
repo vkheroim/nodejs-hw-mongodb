39668ae2149197c11c965416c4fd037d5d3e8fba
vkheroim <vs.kheroim@gmail.com>
2024-08-23 03:46:37 -0300

    Update env.js

--- src/env.js
+++ src/env.js
@@ -16,17 +16,9 @@ export function getEnv(name, defaultValue) {
 const cloudName = getEnv('CLOUD_NAME', 'default_cloud_name');
 
 export const env = {
-  MONGO_URI: 'cluster0.qy82j.mongodb.net',
+  MONGO_URI: 'mongodb+srv://vskheroim:hQP6zw4iCBJ6m1Ss@cluster0.qy82j.mongodb.net',
   PORT: process.env.PORT || 3000,
-  // Add other environment variables as needed
+
 };
-// export function env(name, defaultValue) {
-//   const value = process.env[name];
 
-//   if (value) return value;
-
-//   if (defaultValue) return defaultValue;
-
-//   throw new Error(`Missing: process.env['${name}'].`);
-// }
-MONGO_URI=mongodb+srv://username:password@cluster0.qy82j.mongodb.net/myDatabase?retryWrites=true&w=majority
+MONGO_URI=mongodb+srv://vskheroim:hQP6zw4iCBJ6m1Ss@cluster0.qy82j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
