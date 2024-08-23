5ecdc9c4a9a9d62694ff399b785df216b9044c6f
vkheroim <vs.kheroim@gmail.com>
2024-08-23 03:52:08 -0300

    Update env.js

--- src/env.js
+++ src/env.js
@@ -16,9 +16,8 @@ export function getEnv(name, defaultValue) {
 const cloudName = getEnv('CLOUD_NAME', 'default_cloud_name');
 
 export const env = {
-  MONGO_URI: 'mongodb+srv://vskheroim:hQP6zw4iCBJ6m1Ss@cluster0.qy82j.mongodb.net',
+  MONGO_URI: getEnv('MONGO_URI'),
   PORT: process.env.PORT || 3000,
-
 };
 
 MONGO_URI=mongodb+srv://vskheroim:hQP6zw4iCBJ6m1Ss@cluster0.qy82j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
